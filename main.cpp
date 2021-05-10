ed5c14a4a206460129a28d2bdcdea0dd0ae28c48
long13820 <62087606+long13820@users.noreply.github.com>
2021-05-10 16:14:34 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -49,6 +49,182 @@ struct button
 		void setDoThi();
 		void setThongTin();
 // ====================================================
+//================HAM THUAT TOAN=======================
+//DDA 
+		void lineDDA(int x1,int y1, int x2,int y2);
+//Mid point
+		void put8pixel(int xc, int yc, int x, int y, int color);
+		void drawCircleMidpoint(int xc, int yc, int r, int color);
+//Elipse
+		void Elipse(int x_center,int y_center,int a,int b,int color);
+//Chuyen doi toa do
+		void DoiNguoiDungSangMayTinh(int xDoi, int yDoi,int x0,int y0);
+		void DoiMayTinhSangNguoiDung(int xKQ,int yKQ,int x0,int y0);
+//=====================================================
+//DDA 
+void lineDDA(int x1,int y1, int x2,int y2){
+	int i,j,step;
+	int dx=x2-x1;
+	int dy=y2-y1;
+	abs(dx)>=abs(dy) ? step=abs(dx) : step=abs(dy);
+	float inc_x = (float) dx/step;
+	float inc_y = (float) dy/step;
+	if(abs(dx)>=abs(dy) || abs(dx)<=abs(dy)){
+		float x=x1;
+		float y=y1;
+		for(i=0;i<=abs(step);i++){
+			putpixel(round(x),round(y),c);
+			x+=inc_x;
+			y+=inc_y;
+		}
+	}
+}
+//Mid point
+void put8pixel(int xc, int yc, int x, int y, int color)
+{
+    putpixel(x + xc, y + yc, color);
+    putpixel(-x + xc, y + yc, color);
+    putpixel(x + xc, -y + yc, color);
+    putpixel(-x + xc, -y + yc, color);
+    putpixel( y + xc, x + yc, color);
+    putpixel(-y + xc, x + yc, color);
+    putpixel(y + xc, -x + yc, color);
+    putpixel(-y + xc, -x + yc, color);
+}
+void drawCircleMidpoint(int xc, int yc, int r, int color)
+{
+    int x = 0, y = r;
+    int f = 1 - r;
+	int i = 0;
+	
+    put8pixel(xc, yc, x, y, color);
+	bool first = true;
+    while (x < y)
+    {
+    	
+        if (f < 0) f += (x << 1) + 3;
+        else
+        {
+            y--;
+            f += ((x - y) << 1) + 5;
+        }
+        x++;
+        if(i<=5 && !first){
+        	put8pixel(xc, yc, x, y, color);
+        	
+		}
+		if(i>5){
+			
+		}
+		first = false;
+		if(i==6) i=0;
+		i++;
+        
+    }
+}
+//Elipse
+void Elipse(int x_center,int y_center,int a,int b,int color)
+{
+    float p,a2,b2;
+    int x,y;
+    a2=pow(a,2);
+    b2=pow(b,2);
+    x=0;
+    y=b;
+     
+    p=2*((float)b2/a2)-(2*b)+1;
+     
+    int dem = 0;
+    //ve nhanh thu 1(tu tren xuong )
+    while(((float)b2/a2)*x<=y)
+    {
+		//put pixel
+		if(dem == 3){
+			dem = 0;
+		}
+		else{
+			
+    		putpixel(y_center+x,y_center-y,color);
+    		putpixel(y_center-x,y_center-y,color);
+    		dem++;
+		}
+		putpixel(y_center+x,y_center+y,color);
+    	putpixel(y_center-x,y_center+y,color);
+    	
+    	//thuat toan ve
+        if(p<0)
+        {
+            p=p+2*((float)b2/a2)*(2*x+3);
+            
+        }
+        else{
+            p= p- 4*y + 2*((float)b2/a2)*(2*x+3);
+            y--;
+        }
+        x++;
+    }
+    //ve nhanh thu 2(tu duoi len )
+    y=0;
+    x=a;
+    p=2*((float)a2/b2)-2*a+1;
+    int dem2 = 0;
+    while(((float)a2/b2)*y<=x)
+    {
+    	//put pixel
+    	if(dem == 3){
+			dem = 0;
+		}
+		else{
+			
+    		putpixel(y_center+x,y_center-y,color);
+    		putpixel(y_center-x,y_center-y,color);
+    		dem++;
+		}
+		putpixel(y_center+x,y_center+y,color);
+    	putpixel(y_center-x,y_center+y,color);
+        
+        //thuat toan ve
+        if(p<0)
+        {
+            p=p +2*((float)a2/b2)*(2*y+3);
+        }
+        else
+        {
+            p=p- 4*x + 2*((float)a2/b2)*(2*y+3);
+            x=x-1;
+        }
+        y=y+1;
+    }
+}
+//Chuyen doi toa do
+void DoiNguoiDungSangMayTinh(int xDoi, int yDoi,int x0,int y0){
+	int xKQ,yKQ;
+	if(xDoi>0){
+		xKQ=(xDoi*5)+x0;
+		if(yDoi>0){
+			yKQ=y0-(yDoi*5);
+			cou
+		}
+		else{
+			yKQ=y0+(abs(yDoi)*5);
+		}
+	}
+	else{
+		xKQ=x0-(abs(xDoi)*5);
+		if(yDoi>0){
+			yKQ=y0-(yDoi*5);
+			}
+		else{
+			yKQ = y0+(abs(yDoi)*5);
+		}
+	}
+}
+void DoiMayTinhSangNguoiDung(int xKQ,int yKQ,int x0,int y0){
+	int xDoi,yDoi;
+	
+		xDoi=(xKQ-x0)/5;
+		
+}
 
 // ============ THIET LAP NUT CHUC NANG ===============
 		void taoMangButton(button a[]);
