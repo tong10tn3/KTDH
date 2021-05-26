04f9dfbdb090243e4e4fa354cc543d37602dcf1e
tong10tn3 <56271213+tong10tn3@users.noreply.github.com>
2021-05-26 19:38:32 +0700

    Delete mylib.h

--- mylib.h
+++
@@ -1,8 +0,0 @@
-char* Pwd ();
-int CheckPwd ();
-void gotoxy(short x,short y);
-int wherex( void );
-int wherey( void );
-void TextColor(WORD color);
-void BackGroundColor (WORD color);
-void XoaManHinh();
